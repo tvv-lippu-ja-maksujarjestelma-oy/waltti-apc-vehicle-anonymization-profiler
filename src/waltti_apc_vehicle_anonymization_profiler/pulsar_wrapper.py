94211a57242733d169d146e12bcb59ef3c3ba568
haphut <haphut@mistmap.com>
2023-09-26 15:51:08 +0300

    fix: Allow pulsar to have its own logger

--- src/waltti_apc_vehicle_anonymization_profiler/pulsar_wrapper.py
+++ src/waltti_apc_vehicle_anonymization_profiler/pulsar_wrapper.py
@@ -1,15 +1,21 @@
 """Apache Pulsar functions."""
 import json
+import logging
 
 import pulsar
 
 
-def create_client(logger, client_config, oauth2_config):
+def create_client(
+    logger, client_config, oauth2_config, log_level=logging.INFO
+):
+    # Pulsar is too chatty on level logging.DEBUG.
+    pulsar_logger = logger.getChild()
+    pulsar_logger.setLevel(log_level)
     combined_config = client_config | {
         "authentication": pulsar.AuthenticationOauth2(
             json.dumps(oauth2_config)
         ),
-        "logger": logger,
+        "logger": pulsar_logger,
     }
     return pulsar.Client(**combined_config)
 
