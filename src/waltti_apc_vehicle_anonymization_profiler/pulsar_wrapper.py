ffa7581b65c572ffde5fa820461d432f155b3dd2
Ilkka Lappeteläinen <ilkka.lappetelainen@gmail.com>
2025-10-27 16:46:00 +0200

    Refactor Pulsar wrapper

--- src/waltti_apc_vehicle_anonymization_profiler/pulsar_wrapper.py
+++ src/waltti_apc_vehicle_anonymization_profiler/pulsar_wrapper.py
@@ -14,18 +14,17 @@ def create_client(
     # Pulsar is too chatty on level logging.DEBUG.
     pulsar_logger = logger.getChild("pulsar")
     pulsar_logger.setLevel(log_level)
-    auth_config = (
-        {
+    if oauth2_config is not None:
+        auth_config = {
             "authentication": pulsar.AuthenticationOauth2(
                 json.dumps(oauth2_config)
             )
         }
-        if oauth2_config is not None
-        else {}
+    else:
+        auth_config = {}
+    combined_config = (
+        client_config | auth_config | {"logger": pulsar_logger}
     )
-    combined_config = client_config | auth_config | {
-        "logger": pulsar_logger,
-    }
     return pulsar.Client(**combined_config)
 
 
